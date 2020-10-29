33cb24ead003e6bf0d8b06d8e603f1c7251b691e
Matthew Young <matthewyoung@Matthews-MacBook-Pro.local>
2020-10-29 12:36:33 -0600

    port#10001

--- Webserver.py
+++ Webserver.py
@@ -15,4 +15,4 @@ def main():
     #return render_template('index.html', user_image = cat2)
 
 if __name__ == "__main__":
-    app.run(debug=True, host = "0.0.0.0", port = 80)
+    app.run(debug=True, host = "0.0.0.0", port = 10001)
