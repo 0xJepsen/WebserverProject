29b670a43c78cdddc191a49ba2322c6867c5ec2d
Matt Young <asterix@rams.colostate.edu>
2020-10-29 16:37:16 -0600

    Two different cats, two buttons, working on back end

--- Webserver.py
+++ Webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 from flask_wtf import Form
 import os
 
@@ -7,12 +7,24 @@ IMAGES = os.path.join('static', 'images')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = IMAGES
 
-@app.route("/")
+@app.route("/", methods=['GET', 'VOTE'])
 def main():
+    print(request.method)
+    if request.method=='VOTE':
+        if request.form.get('voteCat1') == 'voteCat1':
+            print("Vote for cat 1")
+            #do stuff
+        elif request.form.get('voteCat2') == 'voteCat2':
+            print("Vote fore cat 2")
+            #do stuff
+        else: 
+            return render_template("index.html")
+    elif request.method=='GET':
+        print("Not Cat")
+
     cat1 = os.path.join(app.config['UPLOAD_FOLDER'], 'smallCat.jpg')
-    #cat2 = os.path.join(app.config['UPLOAD_FOLDER'], 'OrangeCat.jpg')
-    return render_template('index.html', user_image = cat1)
-    #return render_template('index.html', user_image = cat2)
+    cat2 = os.path.join(app.config['UPLOAD_FOLDER'], 'standingCat.pg.jpg')
+    return render_template('index.html', user_image = cat1, user_image2 = cat2)
 
 if __name__ == "__main__":
     app.run(debug=True, host = "0.0.0.0", port = 10001)
